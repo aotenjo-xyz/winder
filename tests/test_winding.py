61de21df0797bee87e66111f4a91775e4d79b4ae
Yuichiro Aoki <yuichiroaoki27@gmail.com>
2026-05-27 12:46:07 +0900

    add integration tests for 12n14p and 36n40p

--- tests/test_winding.py
+++ tests/test_winding.py
@@ -2,24 +2,66 @@ from src.winding import Wind
 
 
 turns = 5  # Use a smaller number for testing
-config_file = "dev-settings.yml"
+config_file_24n22p = "tests/dev-24n22p-settings.yml"
+config_file_12n14p = "tests/dev-12n14p-settings.yml"
+config_file_36n40p = "tests/dev-36n40p-settings.yml"
 
 
-def test_winding_wire0():
-    wind = Wind(config_file, True, turns=turns)
+def test_winding_wire0_24n22p():
+    wind = Wind(config_file_24n22p, True, turns=turns)
     wind.wind(0)
 
 
-def test_winding_wire1():
-    wind = Wind(config_file, True, turns=turns)
+def test_winding_wire1_24n22p():
+    wind = Wind(config_file_24n22p, True, turns=turns)
     wind.wind(1)
 
 
-def test_winding_wire2():
-    wind = Wind(config_file, True, turns=turns)
+def test_winding_wire2_24n22p():
+    wind = Wind(config_file_24n22p, True, turns=turns)
     wind.wind(2)
 
 
-def test_continuous_winding():
-    wind = Wind(config_file, True, turns=turns)
+def test_continuous_winding_24n22p():
+    wind = Wind(config_file_24n22p, True, turns=turns)
+    wind.continuous_winding()
+
+
+def test_winding_wire0_12n14p():
+    wind = Wind(config_file_12n14p, True, turns=turns)
+    wind.wind(0)
+
+
+def test_winding_wire1_12n14p():
+    wind = Wind(config_file_12n14p, True, turns=turns)
+    wind.wind(1)
+
+
+def test_winding_wire2_12n14p():
+    wind = Wind(config_file_12n14p, True, turns=turns)
+    wind.wind(2)
+
+
+def test_continuous_winding_12n14p():
+    wind = Wind(config_file_12n14p, True, turns=turns)
+    wind.continuous_winding()
+
+
+def test_winding_wire0_36n40p():
+    wind = Wind(config_file_36n40p, True, turns=turns)
+    wind.wind(0)
+
+
+def test_winding_wire1_36n40p():
+    wind = Wind(config_file_36n40p, True, turns=turns)
+    wind.wind(1)
+
+
+def test_winding_wire2_36n40p():
+    wind = Wind(config_file_36n40p, True, turns=turns)
+    wind.wind(2)
+
+
+def test_continuous_winding_36n40p():
+    wind = Wind(config_file_36n40p, True, turns=turns)
     wind.continuous_winding()

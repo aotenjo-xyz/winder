4cbe0d6eef389dda4ea07bf692d1524a8708b06a
Yuichiro Aoki <yuichiroaoki27@gmail.com>
2026-05-28 00:16:46 +0900

    Add is_skipping function and corresponding tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,6 +2,7 @@ from src.utils import (
     get_winding_teeth_indices,
     get_current_teeth,
     is_starting_from_bottom,
+    is_skipping,
 )
 
 teeth_indices_24n22p = [
@@ -31,6 +32,33 @@ winding_config_letters_12n14p = "AabBCcaABbcC"  # for 12n14p motor (12 slots, 14
 winding_config_letters_36n40p = (
     "AaABbBCcCAaABbBCcCAaABbBCcCAaABbBCcC"  # for 36n40p motor (36 slots, 40 poles)
 )
+
+
+def test_is_skipping_24n22p():
+    assert not is_skipping(winding_config_letters_24n22p, 0)
+    assert not is_skipping(winding_config_letters_24n22p, 1)
+    assert not is_skipping(winding_config_letters_24n22p, 2)
+    assert not is_skipping(winding_config_letters_24n22p, 3)
+    assert not is_skipping(winding_config_letters_24n22p, 4)
+    assert not is_skipping(winding_config_letters_24n22p, 5)
+    assert not is_skipping(winding_config_letters_24n22p, 6)
+    assert not is_skipping(winding_config_letters_24n22p, 7)
+    assert not is_skipping(winding_config_letters_24n22p, 8)
+    assert not is_skipping(winding_config_letters_24n22p, 9)
+    assert not is_skipping(winding_config_letters_24n22p, 10)
+    assert not is_skipping(winding_config_letters_24n22p, 11)
+    assert is_skipping(winding_config_letters_24n22p, 12)
+    assert not is_skipping(winding_config_letters_24n22p, 13)
+    assert not is_skipping(winding_config_letters_24n22p, 14)
+    assert not is_skipping(winding_config_letters_24n22p, 15)
+    assert is_skipping(winding_config_letters_24n22p, 16)
+    assert not is_skipping(winding_config_letters_24n22p, 17)
+    assert not is_skipping(winding_config_letters_24n22p, 18)
+    assert not is_skipping(winding_config_letters_24n22p, 19)
+    assert is_skipping(winding_config_letters_24n22p, 20)
+    assert not is_skipping(winding_config_letters_24n22p, 21)
+    assert not is_skipping(winding_config_letters_24n22p, 22)
+    assert not is_skipping(winding_config_letters_24n22p, 23)
 
 
 def test_get_winding_teeth_indices_24n22p():
